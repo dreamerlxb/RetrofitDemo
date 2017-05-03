3e6e38e0e988da878e73e40e8b7b2b9af6f12660
lxb <424151385@qq.com>
2017-05-03 13:01:02 +0800

    添加底部导航栏

--- app/src/main/java/com/dreamerlxb/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/dreamerlxb/retrofitdemo/MainActivity.java
@@ -1,10 +1,13 @@
 package com.dreamerlxb.retrofitdemo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.dreamerlxb.retrofitdemo.entity.MarkType;
 import com.dreamerlxb.retrofitdemo.service.MarkTypeService;
 
@@ -16,12 +19,37 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+//import static com.ashokvarma.bottomnavigation.BottomNavigationBar.MODE_FIXED;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar.setBackgroundColor(Color.WHITE);
+        bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+
+        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener(){
+            @Override
+            public void onTabSelected(int position) {
+            }
+            @Override
+            public void onTabUnselected(int position) {
+            }
+            @Override
+            public void onTabReselected(int position) {
+            }
+        });
+
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.mipmap.home_selected, "Home").setInactiveIconResource(R.mipmap.home))
+                .addItem(new BottomNavigationItem(R.mipmap.discover_selected, "Books").setInactiveIconResource(R.mipmap.discover))
+                .addItem(new BottomNavigationItem(R.mipmap.message_selected, "Music").setInactiveIconResource(R.mipmap.message))
+                .addItem(new BottomNavigationItem(R.mipmap.me_selected, "Games").setInactiveIconResource(R.mipmap.me))
+                .initialise();
     }
 
     public void onClick(View view) {
